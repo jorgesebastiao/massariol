fc7319936f21b2af96479853e6b8ba764774ac58
Jorge <jorgenes1@hotmail.com>
2020-07-05 14:26:16 -0300

    ajustado arquivos de environment

--- massariol-client/src/environments/environment.prod.ts
+++ massariol-client/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   apiViaCep: 'https://viacep.com.br/ws/',
-  apiUrl: 'http://certificado.massarioltreinamentos.com.br:8080/massariol-api/v1',
+  apiUrl: 'http://certificado.massarioltreinamentos.com.br/api',
   clientId: 'massariolapiangular',
   secretId: 'm@ss@r1@l@p1@ngul@r',
   tokenName: 'massariolToken',
-  tokenWhitelistedDomains: ['certificado.massarioltreinamentos.com.br:8080'],
+  tokenWhitelistedDomains: ['certificado.massarioltreinamentos.com.br'],
   tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')]
 };
