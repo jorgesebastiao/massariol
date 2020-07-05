a93adcdfa879180aa7d83235dee9b5d59c406b3e
Jorge <jorgenes1@hotmail.com>
2020-07-05 11:34:15 -0300

    Criado modulo para carregar o front-end junto do back-end Criado implementação para permitir o refresh de paginas do front-end Modificado o spring security para permitir todos as requisições exceto a rota que començam com o sufixo /api/** Removido Hateos da aplicação devido a comportamento não esperado

--- massariol-ws/massariol-security/src/main/java/br/com/massariol/security/config/SecurityConfigurer.java
+++ massariol-ws/massariol-security/src/main/java/br/com/massariol/security/config/SecurityConfigurer.java
@@ -17,11 +17,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.expression.OAuth2WebSecurityExpressionHandler;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
+import java.util.Collections;
+
+import static java.util.Arrays.asList;
 
 @Configuration
 @EnableWebSecurity
@@ -49,12 +50,13 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     public void configure(WebSecurity web) {
         web.ignoring().antMatchers("/resources/**", "/v2/api-docs", "/configuration/**", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**");
-        web.ignoring().antMatchers(HttpMethod.OPTIONS,"/**");
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
         web.expressionHandler(new OAuth2WebSecurityExpressionHandler());
     }
 
     public void configure(HttpSecurity http) throws Exception {
-        http.cors();
+        http
+            .cors();
     }
 
     @Bean
@@ -64,18 +66,6 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }
-/*
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
-        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }*/
 
     @Bean
     public FilterRegistrationBean corsFilterRegistrationBean() {
@@ -83,9 +73,9 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
         CorsConfiguration config = new CorsConfiguration();
         config.applyPermitDefaultValues();
         config.setAllowCredentials(true);
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
-        config.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
-        config.setExposedHeaders(Arrays.asList("content-length"));
+        config.setAllowedMethods(asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+        config.setAllowedHeaders(asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+        config.setExposedHeaders(Collections.singletonList("content-length"));
         config.setMaxAge(3600L);
         source.registerCorsConfiguration("/**", config);
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
